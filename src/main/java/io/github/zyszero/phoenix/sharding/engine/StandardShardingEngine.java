e872187d58c159b2cba4ba9141adf60e87436112
zyszero <346124796@qq.com>
2024-08-29 00:09:39 +0800

    replace target sql.

--- src/main/java/io/github/zyszero/phoenix/sharding/engine/StandardShardingEngine.java
+++ src/main/java/io/github/zyszero/phoenix/sharding/engine/StandardShardingEngine.java
@@ -6,16 +6,18 @@ import com.alibaba.druid.sql.ast.SQLName;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
 import com.alibaba.druid.sql.ast.statement.SQLInsertStatement;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
 import io.github.zyszero.phoenix.sharding.config.ShardingProperties;
-import io.github.zyszero.phoenix.sharding.demo.model.User;
 import io.github.zyszero.phoenix.sharding.strategy.HashShardingStrategy;
 import io.github.zyszero.phoenix.sharding.strategy.ShardingStrategy;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Standard sharding engine.
@@ -53,9 +55,11 @@ public class StandardShardingEngine implements ShardingEngine {
     @Override
     public ShardingResult sharding(String sql, Object[] args) {
         SQLStatement sqlStatement = SQLUtils.parseSingleMysqlStatement(sql);
+        String table;
+        Map<String, Object> shardingColumnsMap;
         if (sqlStatement instanceof SQLInsertStatement sqlInsertStatement) {
-            String table = sqlInsertStatement.getTableName().getSimpleName();
-            Map<String, Object> shardingColumnsMap = new HashMap<>();
+            table = sqlInsertStatement.getTableName().getSimpleName();
+            shardingColumnsMap = new HashMap<>();
             List<SQLExpr> columns = sqlInsertStatement.getColumns();
             for (int i = 0; i < columns.size(); i++) {
                 SQLExpr column = columns.get(i);
@@ -63,28 +67,38 @@ public class StandardShardingEngine implements ShardingEngine {
                 String columnName = columnExpr.getSimpleName();
                 shardingColumnsMap.put(columnName, args[i]);
             }
-
-
-            ShardingStrategy databaseStrategy = databaseStrategies.get(table);
-            String targetDatabase = databaseStrategy.doSharding(actualDatabaseNames.get(table), table, shardingColumnsMap);
-
-            ShardingStrategy tableStrategy = tableStrategies.get(table);
-            String targetTable = tableStrategy.doSharding(actualDatabaseNames.get(table), table, shardingColumnsMap);
-            System.out.println(" ===>>> ");
-            System.out.println(" ===>>> target db.table = " + targetDatabase + "." + targetTable);
-            System.out.println(" ===>>> ");
         } else {
-            // TODO SELECT/UPDATE/DELETE
+
+            // SELECT/UPDATE/DELETE
+            MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
+            visitor.setParameters(List.of(args));
+            sqlStatement.accept(visitor);
+
+            LinkedHashSet<SQLName> sqlNames = new LinkedHashSet<>(visitor.getOriginalTables());
+            if (sqlNames.size() > 1) {
+                throw new RuntimeException("no support multi tables sharding: " + sqlNames);
+            }
+            table = sqlNames.iterator().next().getSimpleName();
+            System.out.println(" ===>>> visitor.getOriginalTables = " + table);
+
+            shardingColumnsMap =
+                    visitor.getConditions()
+                            .stream()
+                            .collect(
+                                    Collectors.toMap(
+                                            c -> c.getColumn().getName(), c -> c.getValues().get(0)
+                                    ));
+            System.out.println(" ===>>> visitor.getConditions = " + shardingColumnsMap);
         }
 
-        Object parameterObject = args[0];
-        System.out.println(" ===> getObject sql statements: " + sql);
-        int id = 0;
-        if (parameterObject instanceof User user) {
-            id = user.getId();
-        } else if (parameterObject instanceof Integer userId) {
-            id = userId;
-        }
-        return new ShardingResult(id % 2 == 0 ? "phoenix-sharding0" : "phoenix-sharding1", sql);
+        ShardingStrategy databaseStrategy = databaseStrategies.get(table);
+        String targetDatabase = databaseStrategy.doSharding(actualDatabaseNames.get(table), table, shardingColumnsMap);
+        ShardingStrategy tableStrategy = tableStrategies.get(table);
+        String targetTable = tableStrategy.doSharding(actualDatabaseNames.get(table), table, shardingColumnsMap);
+        System.out.println(" ===>>> ");
+        System.out.println(" ===>>> target db.table = " + targetDatabase + "." + targetTable);
+        System.out.println(" ===>>> ");
+
+        return new ShardingResult(targetDatabase, sql.replace(table, targetTable));
     }
 }
--- src/main/java/io/github/zyszero/phoenix/sharding/mybatis/SqlStatementInterceptor.java
+++ src/main/java/io/github/zyszero/phoenix/sharding/mybatis/SqlStatementInterceptor.java
@@ -1,11 +1,17 @@
 package io.github.zyszero.phoenix.sharding.mybatis;
 
+import io.github.zyszero.phoenix.sharding.engine.ShardingContext;
+import io.github.zyszero.phoenix.sharding.engine.ShardingResult;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
+import org.springframework.objenesis.instantiator.util.UnsafeUtils;
 import org.springframework.stereotype.Component;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
 
 /**
  * interceptor sql.
@@ -24,12 +30,25 @@ public class SqlStatementInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler handler = (StatementHandler) invocation.getTarget();
-        BoundSql boundSql = handler.getBoundSql();
-        System.out.println(" ===> SqlStatementInterceptor: " + boundSql.getSql());
-        Object parameterObject = boundSql.getParameterObject();
-        System.out.println(" ===> SqlStatementInterceptor: " + parameterObject);
-        // todo 修改 sql，比如 user -> user1
+        ShardingResult result = ShardingContext.get();
+        if (result != null) {
+            StatementHandler handler = (StatementHandler) invocation.getTarget();
+            BoundSql boundSql = handler.getBoundSql();
+            String sql = boundSql.getSql();
+            System.out.println(" ===> SqlStatementInterceptor: " + sql);
+            String targetSql = result.getTargetSqlStatement();
+            if (!sql.equalsIgnoreCase(targetSql)) {
+                replaceSql(boundSql, targetSql);
+                System.out.println(" ===> target sql: " + targetSql);
+            }
+        }
         return invocation.proceed();
+    }
+
+    private static void replaceSql(BoundSql boundSql, String targetSql) throws NoSuchFieldException {
+        Field field = boundSql.getClass().getDeclaredField("sql");
+        Unsafe unsafe = UnsafeUtils.getUnsafe();
+        long fieldOffset = unsafe.objectFieldOffset(field);
+        unsafe.putObject(boundSql, fieldOffset, targetSql);
     }
 }
